7d23311910e049ee4afd02d1c0259acf70ce3076
Sebastian <github.constant352@passmail.net>
2025-07-10 01:56:17 +0200

    ktor seems to work. I get a 200 OK status back

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 package org.example
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    println("Hello, World!")
+suspend fun main() {
+    val client = HttpClient(CIO)
+
+    val response: HttpResponse = client.get("https://api.spacetraders.io/v2")
+
+    println(response.status)
+
+
 }
